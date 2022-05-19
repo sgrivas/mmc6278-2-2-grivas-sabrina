4df7843dff230315787f7133588d10a596e84de1
Sabrina Grivas <sgrivas1@gmail.com>
2022-05-19 08:20:15 -0400

    prompt user for name and writing final if

--- index.js
+++ index.js
@@ -2,4 +2,15 @@
 function greet() {
     var name = prompt("What is your name?");
     alert(`Hello, ${name}`);
+    var age = prompt("How old are you?");
+    var ageNumber= parseInt(age);
+    var birthday = confirm("Have you had a birthday yet this year?");
+    var currentYear = new Date().getFullYear();
+    var birthdayThisYear = currentYear-ageNumber;
+    var birthdayNot = currentYear-(ageNumber+1);
+    if (birthday){
+        alert(`You were born in ${birthdayThisYear}`)
+    } else {
+        alert(`You were born in ${birthdayNot}`)
+    }
 }
